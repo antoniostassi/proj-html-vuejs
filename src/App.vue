2b25f41bd127c20d99fe68aed4aaf73fe31795f0
Antonio Stassi <stassiant@outlook.it>
2024-09-19 19:24:01 +0200

    Initialising Repository w/ template

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script>
+    import TestTemplate from './components/TestTemplate.vue';
+    export default {
+        data(){
+            return{
+                
+            }
+        },
+        components:{
+            TestTemplate
+        }
+    }
+</script>
+
+<template>
+    <h1>Test</h1>
+    <TestTemplate/>
+</template>
+
+<style lang="scss" scoped>
+    @import "bootstrap/scss/bootstrap";
+    
+</style>
