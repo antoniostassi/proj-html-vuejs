adb65195e600836ea9a251bb48891b7e493f8bf8
Antonio Stassi <stassiant@outlook.it>
2024-09-20 12:25:01 +0200

    Macro categories

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -9,19 +9,28 @@
 </script>
 
 <template>
-    <img src="/src/assets/images/avada-bakery-logo.png" alt="Logo">
-    <ul>
-        <li><a href="#">Home</a></li> <!-- Header list will be here -->
-    </ul>
 
-    <section>
-        <div class="container-xxl">
-            <!-- First section "The Perfect Fresh Bread"-->
-            <h2>The Perfect Fresh Bread</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, fugit. Unde asperiores porro natus, dolores magnam quo quas fugiat architecto.</p>
-            <button>Explore our products</button>
-        </div>
-    </section>
+    <header>
+
+        <!-- Logo + NavBar-->
+        <img src="/src/assets/images/avada-bakery-logo.png" alt="Logo">
+
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li> <!-- Header list will be here -->
+            </ul>
+        </nav>
+        
+        <!-- First section "The Perfect Fresh Bread"-->
+        <section>
+            <div class="container-xxl">
+                <h2>The Perfect Fresh Bread</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, fugit. Unde asperiores porro natus, dolores magnam quo quas fugiat architecto.</p>
+                <button>Explore our products</button>
+            </div>
+        </section>
+    </header>
+    
 </template>
 
 <style lang="scss" scoped>
