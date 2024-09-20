c43eafd511f89721efba73e9829b06984d09beea
Antonio Stassi <stassiant@outlook.it>
2024-09-20 12:20:23 +0200

    Header component

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -13,6 +13,15 @@
     <ul>
         <li><a href="#">Home</a></li> <!-- Header list will be here -->
     </ul>
+
+    <section>
+        <div class="container-xxl">
+            <!-- First section "The Perfect Fresh Bread"-->
+            <h2>The Perfect Fresh Bread</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, fugit. Unde asperiores porro natus, dolores magnam quo quas fugiat architecto.</p>
+            <button>Explore our products</button>
+        </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
