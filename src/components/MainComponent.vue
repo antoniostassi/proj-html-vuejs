a281e65934b0a4e552f1e74782d2bf120f13a2b1
Antonio Stassi <stassiant@outlook.it>
2024-09-20 12:35:54 +0200

    Main component skeleton

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,91 @@
+<script>
+    export default {
+        data(){
+            return{
+
+            }
+        }
+    }
+</script>
+
+<template>
+    <main>
+        <div class="container-xxl">
+
+            <section>
+                <div class="row">
+                    <div class="col-5">
+                        <!-- our producs-->
+                        <span>OUR PRODUCTS</span>
+                        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Debitis, dolorem.</p>
+                        <button>Start Shopping</button>
+                    </div>
+
+                    <div class="col-7">
+                        <div class="carousel two-images">
+                            <!-- first carousel -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+            <section> <!-- Wedding -->
+                <div class="text-center">
+                    <span>CORPORATE & WEDDINGS</span>
+                    <h1>Baking Special Moments</h1>
+                </div>
+
+                <div class="row">
+                    <div class="col-6">
+                        <!-- first image -->
+                    </div>
+                    <div class="col-6">
+                        <!-- second image -->
+                    </div>
+                </div>
+            </section>
+            
+            <section> <!-- Biscuits Prices -->
+                <div class="row">
+                    <div class="col-4 text-center">
+                        <h2>Find a freshly baked product perfect for you</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, cupiditate?</p>
+                        <button>Shop All Products</button>
+                    </div>
+                    <div class="col-8">
+                        <div class="carousel four-img">
+                            <!-- shop carousel -->
+                        </div>
+                    </div>
+                </div>
+            </section>
+            
+            <section> <!-- big images section -->
+
+            </section>
+
+            <section> <!-- stores locations -->
+
+            </section>
+
+            <section> <!-- social images -->
+
+            </section>
+
+        </div>
+        
+        <section> <!-- Last section -->
+            <div class="big-bg">
+                <div class="container">
+                    <span>TRY OUR SEASONAL PRODUCTS</span>
+                    <h2>Order for pick-up or delivery to your home</h2>
+                    <button>Shop Now</button>
+                </div>
+            </div>
+        </section>
+    </main>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
