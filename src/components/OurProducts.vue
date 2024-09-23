62b51c4a4d0a4ffde3265bf8cbbe9f3f10bd83cf
Antonio Stassi <stassiant@outlook.it>
2024-09-23 15:09:12 +0200

    Section 1 Carousel working properly ( Forward and Backward showing 2 images at the same time )

--- src/components/OurProducts.vue
+++ src/components/OurProducts.vue
@@ -1,20 +1,48 @@
 <script>
+    import {store} from '../store';
     export default {
         data(){
             return{
-
+                store,
+                currentCarouselImg: 0,
+                secondCarouselImg: 1
+            }
+        },
+        methods:{ 
+            arrowBackward() {
+                if (this.currentCarouselImg == 0) {
+                    this.currentCarouselImg = store.ourProducts.length-1
+                } else {
+                    this.currentCarouselImg--;
+                };
+                this.getNxtImage();
+            },
+            arrowForward() {
+                if (this.currentCarouselImg == this.store.ourProducts.length-1) {
+                    this.currentCarouselImg = 0
+                } else {
+                    this.currentCarouselImg++;
+                };
+                this.getNxtImage();
+            },
+            getNxtImage() {
+                if (this.currentCarouselImg === this.store.ourProducts.length-1) {
+                    this.secondCarouselImg = 0;
+                } else {
+                    this.secondCarouselImg = this.currentCarouselImg+1 ;
+                }
             }
         }
     }
 </script>
 
 <template>
-    <section class="our-products">
+    <section class="our-products pb-5">
         <div class="row">
-            <div class="col-5">
+            <div class="col-5 ps-3">
                 <!-- our producs-->
                 <span class="serif-regular small-title light-font">OUR PRODUCTS</span>
-                <h2 class="serif-bold dark-font mt-2 mb-5">
+                <h2 class="serif-bold dark-font mt-3 mb-5">
                     All our delectable pastries are backed fresh in our Kitchen every morning, and are made with all-natural, 
                     all organic ingredients.
                 </h2>
@@ -25,8 +53,15 @@
             </div>
 
             <div class="col-7">
-                <div class="carousel two-images">
-                    <!-- first carousel -->
+                <div class="carousel w-100 d-flex border position-relative">
+                    <div class="backward-img light-font" @click="arrowBackward()">
+                        <i class="fa-solid fa-circle-chevron-left"></i>
+                    </div>
+                    <div class="forward-img light-font" @click="arrowForward()">
+                        <i class="fa-solid fa-circle-chevron-right"></i>
+                    </div>
+                    <div class="image" :style="{backgroundImage:`url('/src/assets/images/${store.ourProducts[currentCarouselImg].productImg}')`}"></div>
+                    <div class="image" :style="{backgroundImage:`url('/src/assets/images/${store.ourProducts[secondCarouselImg].productImg}')`}"></div>
                 </div>
             </div>
         </div>
---
+++ src/store.js
@@ -0,0 +1,49 @@
+import { reactive } from 'vue';
+export const store = reactive({
+
+    ourProducts: [
+        {
+            productName: "Choco Chip Cookies",
+            productCategory: "Cookies, Pastries",
+            productPrice:"$19.00 - $39.00",
+            productImg: "choco-chip-cookies.jpg"
+        },
+        {
+            productName: "Straberry Jam Cookies",
+            productCategory: "Cookies, Pastries",
+            productPrice:"$24.00 - $62.00",
+            productImg: "strawberry-jam-cookies.jpg"
+        },
+        {
+            productName: "Strawberry Donut",
+            productCategory: "Donuts, Pastries",
+            productPrice:"$24.00 - $42.00",
+            productImg: "strawberry-donut.jpg"
+        },
+        {
+            productName: "Perfect Macarons",
+            productCategory: "Cookies, Pastries",
+            productPrice:"$18.00 - $52.00",
+            productImg:"perfect-macarons.jpg"
+        },
+        {
+            productName: "Home Bread",
+            productCategory: "Bread, Bakery",
+            productPrice:"$5.00 - $15.00",
+            productImg:"home-bread.jpg"
+        },
+        {
+            productName: "Glazed Pancake with Lemon",
+            productCategory: "Cakes, Pastries",
+            productPrice:"$19.00",
+            productImg:"glazed-pancake-with-lemon.jpg"
+        },
+        {
+            productName: "Blackberry Stuffed Bread",
+            productCategory: "Bread, Bakery",
+            productPrice:"$12.00 - $20.00",
+            productImg:"blackberry-stuffed-bread.jpg"
+        }
+    ]
+
+});
