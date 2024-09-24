69301e4c174ba78fcb44d4cfdead6ac7246ccace
Antonio Stassi <stassiant@outlook.it>
2024-09-24 14:53:00 +0200

    + section semantic tag

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -9,36 +9,40 @@
 </script>
 
 <template>
-    <div class="row social mt-5 pt-5">
-        <div class="col-3">
-            <div class="first w-100 h-100"></div>
-        </div>
-        <div class="col-6">
-            <div class="panel w-100 h-100 text-center">
-                <span class="serif-regular light-font small-title">MADE FOR SHARING</span>
-                <h2 class="serif-bold dark-font mt-3">Let's stay in touch</h2>
-                <div class="bakery-btn m-auto mt-4 btn-bg-color px-4 py-2 rounded serif-regular"><i class="fa-brands fa-instagram me-2"></i>Follow us</div>
+
+    <section>
+        <div class="row social mt-5 pt-5">
+            <div class="col-3">
+                <div class="first w-100 h-100"></div>
+            </div>
+            <div class="col-6">
+                <div class="panel w-100 h-100 text-center">
+                    <span class="serif-regular light-font small-title">MADE FOR SHARING</span>
+                    <h2 class="serif-bold dark-font mt-3">Let's stay in touch</h2>
+                    <div class="bakery-btn m-auto mt-4 btn-bg-color px-4 py-2 rounded serif-regular"><i class="fa-brands fa-instagram me-2"></i>Follow us</div>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="second w-100 h-100"></div>
             </div>
         </div>
-        <div class="col-3">
-            <div class="second w-100 h-100"></div>
-        </div>
-    </div>
 
-    <div class="row social mt-3">
-        <div class="col-3">
-            <div class="third w-100 h-100"></div>
+        <div class="row social mt-3">
+            <div class="col-3">
+                <div class="third w-100 h-100"></div>
+            </div>
+            <div class="col-3">
+                <div class="fourth w-100 h-100"></div>
+            </div>
+            <div class="col-3">
+                <div class="fifth w-100 h-100"></div>
+            </div>
+            <div class="col-3">
+                <div class="sixth w-100 h-100"></div>
+            </div>
         </div>
-        <div class="col-3">
-            <div class="fourth w-100 h-100"></div>
-        </div>
-        <div class="col-3">
-            <div class="fifth w-100 h-100"></div>
-        </div>
-        <div class="col-3">
-            <div class="sixth w-100 h-100"></div>
-        </div>
-    </div>
+    </section>
+    
 </template>
 
 <style lang="scss" scoped>
